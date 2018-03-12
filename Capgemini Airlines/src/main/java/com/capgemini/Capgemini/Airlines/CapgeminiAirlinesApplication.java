9e63e18175f8063b181b4222414f295069f0ebfa
Daniëlle Duijst <dmsduijst@gmail.com>
2018-03-12 09:19:36 +0100

    Initial commit

---
+++ Capgemini Airlines/src/main/java/com/capgemini/Capgemini/Airlines/CapgeminiAirlinesApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.Capgemini.Airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgeminiAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgeminiAirlinesApplication.class, args);
+	}
+}
